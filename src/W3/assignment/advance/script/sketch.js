ebe92558c86238b140cf6a590a01e18cf3ea3578
DEPAL1237 <daepha818@gmail.com>
2023-09-25 10:39:34 +0900

    3주차 과제

--- src/W3/assignment/advance/script/sketch.js
+++ src/W3/assignment/advance/script/sketch.js
@@ -1,12 +1,59 @@
-function setup() {
-  setCanvasContainer('anyname', 3, 2, true);
+let pos;
+let vel;
+let acc;
+let mv;
 
-  background(255);
-  noStroke();
-  fill('red');
+function setup() {
+  let canvas;
+  canvas = createCanvas(700, 700);
+  let canvasParent;
+  canvasParent = select('#mySketchGoesHere');
+  canvas.parent(canvasParent);
+  background('#F3EFFF');
+
+  mv = createVector();
+
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  acc = createVector(0, 0);
 }
 
 function draw() {
-  background(255);
-  circle(mouseX, mouseY, 50);
+  background('#F3EFFF');
+  display();
+  displayVectors();
+
+  vel.add(acc);
+  pos.add(vel);
+  vel.limit(5);
+
+  let mouse = createVector(mouseX, mouseY);
+  strokeWeight(2);
+  stroke('black');
+  line(pos.x, pos.y, mouseX, mouseY);
+
+  mv.x = mouseX;
+  mv.y = mouseY;
+
+  let cacc = p5.Vector.sub(mouse, pos);
+  cacc.setMag(0.1);
+  acc = cacc;
+
+  if (mouseIsPressed) {
+    let clickPosition = createVector(mouseX, mouseY);
+    let cacc = p5.Vector.sub(pos, clickPosition);
+    cacc.setMag(15);
+    pos.add(cacc);
+  }
+}
+
+function display() {
+  noStroke();
+  fill('red');
+  ellipse(pos.x, pos.y, 60);
+}
+
+function displayVectors() {
+  stroke('blue');
+  line(pos.x, pos.y, pos.x + vel.x * 10, pos.y + vel.y * 10);
 }
--- src/W3/assignment/basic/script/sketch.js
+++ src/W3/assignment/basic/script/sketch.js
@@ -1,66 +1,52 @@
+let pos;
+let vel;
+let acc;
+let mv;
+
 function setup() {
-  setCanvasContainer('p5-canvas', 1, 1, true);
-  background('white');
+  let canvas;
+  canvas = createCanvas(700, 700);
+  let canvasParent;
+  canvasParent = select('#mySketchGoesHere');
+  canvas.parent(canvasParent);
+  background('#F3EFFF');
+
+  mv = createVector();
+
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  acc = createVector(0, 0);
 }
 
 function draw() {
-  background(255);
-  rectMode(CORNER);
-  fill(255);
-  colorMode(RGBA);
-  //   stroke(0);
-  //   strokeWeight(1);
+  background('#F3EFFF');
+  display();
+  displayVectors();
+
+  vel.add(acc);
+  pos.add(vel);
+  vel.limit(5);
+
+  let mouse = createVector(mouseX, mouseY);
+  strokeWeight(2);
+  stroke('black');
+  line(pos.x, pos.y, mouseX, mouseY);
+
+  mv.x = mouseX;
+  mv.y = mouseY;
+
+  let cacc = p5.Vector.sub(mouse, pos);
+  cacc.setMag(0.1);
+  acc = cacc;
+}
+
+function display() {
   noStroke();
+  fill('red');
+  ellipse(pos.x, pos.y, 60);
+}
 
-  //벽
-  fill('#0067a3');
-  rect(0, 0, 1000);
-
-  ///창문
-  fill('#aa9999');
-  rect(70, 20, 300, 400);
-
-  ////풍경
-  fill('#4c5a66');
-  rect(80, 30, 280, 380);
-  fill('#384254');
-
-  rect(80, 80, 160, 330);
-  rect(200, 160, 160, 250);
-  fill('#262d35');
-  rect(80, 80, 30, 330);
-  fill('#262d35');
-  rect(240, 160, 30, 250);
-
-  //불
-  fill('#e0d59b');
-  rect(110, 120, 60, 20);
-  rect(190, 170, 40, 20);
-  rect(290, 220, 30, 20);
-  rect(150, 300, 80, 20);
-  rect(200, 100, 30, 20);
-
-  noStroke();
-
-  //책장
-  fill('#cc8787');
-  rect(500, 220, 700, 1000);
-  fill('#cc6666');
-  rect(525, 270, 1000, 700);
-  fill('#cc9999');
-  rect(525, 370, 1000, 15);
-  rect(525, 370 + 1 * 115, 1000, 15);
-  rect(525, 370 + 2 * 115, 1000, 15);
-  rect(525, 370 + 3 * 115, 1000, 15);
-  rect(525, 370 + 4 * 115, 1000, 15);
-
-  //책상
-  fill(255);
-  ////위
-  fill('#cc9999');
-  rect(0, 670, 450, 25);
-  ////다리
-  fill('#8e6666');
-  rect(90, 695, 25, 280);
-  rect(410, 695, 25, 280);
+function displayVectors() {
+  stroke('blue');
+  line(pos.x, pos.y, pos.x + vel.x * 10, pos.y + vel.y * 10);
 }
