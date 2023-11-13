b132a61c19c4aed59f03813d5b3cddf821e12e1c
DEPAL1237 <daepha818@gmail.com>
2023-11-13 22:07:40 +0900

    모르겠다...ㅋ

--- src/W9/assignment/basic/script/sketch.js
+++ src/W9/assignment/basic/script/sketch.js
@@ -1,66 +1,202 @@
-const cNum = 8;
-const rNum = 8;
+const oWidth = 800;
+const oHeight = 600;
 
-let rad = 0;
-let angle = 0;
+// 기본 변수 선언
+const {
+  Engine,
+  Render,
+  Runner,
+  Body,
+  Composite,
+  Composites,
+  Constraint,
+  MouseConstraint,
+  Mouse,
+  Bodies,
+  Common,
+  Vertices,
+} = Matter;
+
+//decomp 사용
+Common.setDecomp(decomp);
+
+// create engine
+const engine = Engine.create(),
+  world = engine.world;
+
+// create runner
+const runner = Runner.create();
+Runner.run(runner, engine);
+const walls = [];
+
+let group;
+let ropeA;
+let ropeB;
+let ropeC;
+
+let mouse;
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-  rad = width / 25;
-  console.log('rad', rad);
-  console.log('width', width);
+  setCanvasContainer('canvas', oHeight, oHeight, true);
 
-  colorMode(HSL, 360, 100, 100, 100);
-  background(360, 0, 100);
+  let concave1 = Vertices.fromPath('-24 0 -10 -18 29 -6 7 7 4 32 -20 23'),
+    chevron = Vertices.fromPath(
+      '33.33 0 23.33 16.67 33.33 33.33 6.67 33.33 0 16.67 3.33 0'
+    );
+  arrow = Vertices.fromPath('20 0 20 10 50 10 50 40 20 40 20 50 0 25');
+  group = Matter.Body.nextGroup(true);
+
+  ropeA = Matter.Composites.stack(100, 50, 8, 1, 10, 10, function (x, y) {
+    return Matter.Bodies.fromVertices(x, y, concave1, {
+      collisionFilter: { group: group },
+    });
+  });
+
+  Matter.Composites.chain(ropeA, 0.5, 0, -0.5, 0, {
+    stiffness: 0.8,
+    length: 2,
+    render: { type: 'line' },
+  });
+
+  Matter.Composite.add(
+    ropeA,
+    Matter.Constraint.create({
+      bodyB: ropeA.bodies[0],
+      pointB: { x: -25, y: 0 },
+      pointA: { x: ropeA.bodies[0].position.x, y: ropeA.bodies[0].position.y },
+      stiffness: 0.5,
+    })
+  );
+
+  group = Matter.Body.nextGroup(true);
+
+  ropeB = Matter.Composites.stack(350, 50, 10, 1, 10, 10, function (x, y) {
+    return Matter.Bodies.fromVertices(x - 20, y, chevron, {
+      collisionFilter: { group: group },
+    });
+  });
+
+  Matter.Composites.chain(ropeB, 0.5, 0, -0.5, 0, {
+    stiffness: 0.8,
+    length: 2,
+    render: { type: 'line' },
+  });
+
+  Matter.Composite.add(
+    ropeB,
+    Matter.Constraint.create({
+      bodyB: ropeB.bodies[0],
+      pointB: { x: -20, y: 0 },
+      pointA: { x: ropeB.bodies[0].position.x, y: ropeB.bodies[0].position.y },
+      stiffness: 0.5,
+    })
+  );
+
+  group = Matter.Body.nextGroup(true);
+
+  ropeC = Matter.Composites.stack(600, 50, 13, 1, 10, 10, function (x, y) {
+    return Matter.Bodies.fromVertices(x - 20, y, arrow, {
+      collisionFilter: { group: group },
+      chamfer: 5,
+    });
+  });
+
+  Matter.Composites.chain(ropeC, 0.3, 0, -0.3, 0, { stiffness: 1, length: 0 });
+
+  Matter.Composite.add(
+    ropeC,
+    Matter.Constraint.create({
+      bodyB: ropeC.bodies[0],
+      pointB: { x: -20, y: 0 },
+      pointA: { x: ropeC.bodies[0].position.x, y: ropeC.bodies[0].position.y },
+      stiffness: 0.5,
+    })
+  );
+
+  //요소를 세계에 추가하기
+  Matter.Composite.add(world, [
+    ropeA,
+    ropeB,
+    ropeC,
+    Matter.Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true }),
+  ]);
+
+  // 마우스 컨트롤 추가하기
+  mouse = Mouse.create(canvas.elt);
+  mouse.pixelRatio = (pixelDensity() * width) / oWidth;
+  let mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+    },
+  });
+
+  Composite.add(world, mouseConstraint);
+
+  background('#3F3A4A');
+  Runner.run(runner, engine);
+
+  //확인
+  console.log('group', group);
+  console.log('ropeA', ropeA);
+  console.log('ropeB', ropeB);
+  console.log('ropeC', ropeC);
+  console.log('Bodies', Bodies);
 }
 
 function draw() {
-  background(360, 0, 100);
-  angleVel = (TAU / 360) * 15;
+  mouse.pixelRatio = (pixelDensity() * width) / oWidth;
 
-  angle += (TAU / 360) * 1;
-  for (let r = 0; r < rNum; r++) {
-    for (let c = 0; c < cNum; c++) {
-      let x = r * (width / (rNum + 1 / 3)) + rad * 2;
-      let y = c * (width / (cNum + 1 / 3)) + rad * 2;
-      let angleStepC = r * (TAU / 360) * 15;
+  background('#3F3A4A');
+  // scale(width / oWidth, height / oHeight);
+  colorMode(HSL);
+  stroke(54, 90, 80);
+  fill(54, 90, 80);
+  ropeA.bodies.forEach((eachBody) => {
+    eachBody.parts.forEach((eachPart, idx) => {
+      if (idx === 0) return;
+      beginShape();
+      eachPart.vertices.forEach((eachVertex) => {
+        vertex(
+          (eachVertex.x / oWidth) * width,
+          (eachVertex.y / oHeight) * height
+        );
+      });
+      endShape(CLOSE);
+    });
+  });
 
-      let angleStepR = rNum * c * (TAU / 360) * 15;
-      push();
-      translate(x, y);
-      rotate(angle + angleStepC + angleStepR);
+  colorMode(HSL);
+  stroke(100, 90, 80);
+  fill(100, 90, 80);
+  ropeB.bodies.forEach((eachBody) => {
+    eachBody.parts.forEach((eachPart, idx) => {
+      if (idx === 0) return;
+      beginShape();
+      eachPart.vertices.forEach((eachVertex) => {
+        vertex(
+          (eachVertex.x / oWidth) * width,
+          (eachVertex.y / oHeight) * height
+        );
+      });
+      endShape(CLOSE);
+    });
+  });
 
-      if (r % 2 == 0 && c % 2 == 0) {
-        fill(306, 100, 74, 60);
-        stroke(0);
-        strokeWeight(2.5);
-        ellipse(0, 0, rad * 2);
-        line(0, 0, 20, 20);
-      } else if (r % 2 == 0 && c % 2 == 1) {
-        fill(47, 100, 69, 60);
-        stroke(0);
-        strokeWeight(2.5);
-        ellipse(0, 0, rad * 2);
-        line(0, 0, 20, 20);
-      } else {
-        if (c % 2 == 1) {
-          fill(194, 100, 69, 60);
-          stroke(0);
-          strokeWeight(2.5);
-          ellipse(0, 0, rad * 2);
-          line(0, 0, 20, 20);
-        } else {
-          fill(133, 100, 69, 60);
-          stroke(0);
-          strokeWeight(2.5);
-          ellipse(0, 0, rad * 2);
-          line(0, 0, 20, 20);
-        }
-      }
-
-      fill(255);
-      ellipse(20, 20, 10);
-      pop();
-    }
-  }
+  colorMode(HSL);
+  stroke(250, 90, 80);
+  fill(250, 90, 80);
+  ropeC.bodies.forEach((eachBody) => {
+    eachBody.parts.forEach((eachPart, idx) => {
+      if (idx === 0) return;
+      beginShape();
+      eachPart.vertices.forEach((eachVertex) => {
+        vertex(
+          (eachVertex.x / oWidth) * width,
+          (eachVertex.y / oHeight) * height
+        );
+      });
+      endShape(CLOSE);
+    });
+  });
 }
